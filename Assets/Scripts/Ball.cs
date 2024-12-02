f996e602339d92e363734bfdde43055f2c62cc8a
naufalizzuddin <naufaltaufikh3@gmail.com>
2024-12-02 16:14:13 +0700

    first

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public Racket LeftRacket, RightRacket;
+    public Rigidbody2D ballRb;
+    public float moveSpeed;
+    public GameObject Update1, Update2;
+
+    void Start()
+    {
+        ballRb.velocity = new Vector2(1, 1) * moveSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        TagMenager tagMenager = collision.gameObject.GetComponent<TagMenager>();
+
+        GetComponent<AudioSource>().Play();
+
+        if (tagMenager == null)
+        {
+            return;
+        }
+
+        Tag tag = tagMenager.wallTag;
+
+        if (tag.Equals(Tag.leftWall))
+        {
+            RightRacket.GetScore();
+            StartCoroutine(ShowUpdateText(Update2));
+        }
+
+        if (tag.Equals(Tag.rightWall))
+        {
+            LeftRacket.GetScore();
+            StartCoroutine(ShowUpdateText(Update1));
+        }
+        if (tag.Equals(Tag.leftRacket))
+        {
+            wayBall(collision, 1);
+        }
+        if (tag.Equals(Tag.rightRacket))
+        {
+            wayBall(collision, -1);
+        }
+    }
+
+    private void wayBall(Collision2D collision, int x)
+    {
+        float a = transform.position.y - collision.gameObject.transform.position.y;
+        float b = collision.collider.bounds.size.y;
+        float y = a / b;
+        ballRb.velocity = new Vector2(x, y) * moveSpeed;
+    }
+
+    private IEnumerator ShowUpdateText(GameObject updateText)
+    {
+    updateText.SetActive(true);
+    yield return new WaitForSeconds(1.0f);
+    updateText.SetActive(false);
+    }
+
+}
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    public void Load(int index)
+    {
+        SceneManager.LoadScene(index);
+        Time.timeScale = 1f;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Reset1()
+    {
+        Load(1);
+    }
+    public void Reset2()
+    {
+        Load(2);
+    }
+    public void Reset3()
+    {
+        Load(3);
+    }
+}
